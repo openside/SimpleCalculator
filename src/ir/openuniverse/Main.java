ed50a04a577c4183fea8c9a8963ad42995ef1db0
OpenSide Edu User <SM.MirEsmaeely@gmail.com>
2018-11-03 21:38:42 +0330

    First successful launch

--- src/ir/openuniverse/Main.java
+++ src/ir/openuniverse/Main.java
@@ -1,8 +1,20 @@
 package ir.openuniverse;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        final Scanner scanner = new Scanner(System.in);
+    
+        System.out.print("Enter first number: ");
+        int a = scanner.nextInt();
+    
+        System.out.print("Enter Second number: ");
+        int b = scanner.nextInt();
+    
+        int c = a + b;
+    
+        System.out.println("The sum is " + c + ".");
     }
 }
